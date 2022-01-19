f3b4ae863584c95411ed63408381e77dc55305f1
Debarghaya Dasgupta <gyaansastra@gmail.com>
2022-01-19 22:10:31 +1100

    action changes

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,3 +4,4 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 sudo apt install docker-ce -y
+sudo apt install docker-compose -y
