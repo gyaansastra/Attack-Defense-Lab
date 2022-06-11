dc432c108ac643910025f961848cdea24f57d6ab
Debarghaya Dasgupta <gyaansastra@gmail.com>
2022-06-11 14:56:19 +1000

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,3 +5,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 sudo apt install docker-ce -y
 sudo apt install docker-compose -y
+sudo usermod -aG docker $USER
